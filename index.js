3ca816e2406deda9b444ce12e4d793deadc68d83
aun ahmad <ahmadaun90@gmail.com>
2023-07-06 01:24:42 +0530

    fixed nav animation

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const openBtn = document.querySelector(".open-icon");
 const closeBtn = document.querySelector(".close-icon");
 
 openBtn.addEventListener("click", () => {
-  overlay.classList.remove("close");
   overlay.classList.toggle("open");
 });
 closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("open");
-  overlay.classList.add("close");
+  overlay.classList.toggle("open");
 });
